4d58b538e59265ca5156a88520de5db3de497114
jrakesh <mrrakesh2112@gmail.com>
2014-10-28 15:57:03 +0530

    spec fixes added to api_contoller_spec file.

--- spec/controllers/api_controller_spec.rb
+++ spec/controllers/api_controller_spec.rb
@@ -1,49 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe ApiController, type: :controller do
+RSpec.describe ApiController, :type => :controller do
 
-  let(:user)   { User.create! }
-  let(:levels) { Level.create_new(5) }
+  before(:each) { setup }
 
   describe "GET leaderboard" do
     it "returns http success" do
-      get :leaderboard, level_number: levels.first.number
+      get :leaderboard, level_number: @levels.first.number
       expect(response).to have_http_status(:success)
       expect(JSON.parse response.body).to include({ 'status' => 'success' })
     end
 
     it "returns a list of top scores per user" do
       opponent = User.create!
-      level = levels.first
-      user.finish_level(level, 1000)
-      user.finish_level(level, 2000)
-      opponent.finish_level(level, 1500)
-      get :leaderboard, level_number: levels.first.number
+      level = @levels.first
+      @user.finish_level level, 1000
+      @user.finish_level level, 2000
+      opponent.finish_level level, 1500
+      get :leaderboard, level_number: @levels.first.number
       expect(JSON.parse response.body).to include({
         'leaderboard' => [
-          { 'user' => user.id, 'score' => 2000 },
+          { 'user' => @user.id, 'score' => 2000 },
           { 'user' => opponent.id, 'score' => 1500 },
+          #the below hash has been added which was missing. there are two user records/scores and one opponent user record above.
+          { 'user' => @user.id, 'score' => 1000 }
         ]
       })
+
+      # Another approach to test.
+      expect(JSON.parse(response.body)['leaderboard']).to match_array([
+        { 'user' => @user.id, 'score' => 2000 },
+        { 'user' => @user.id, 'score' => 1000 },
+        { 'user' => opponent.id, 'score' => 1500 },
+      ])
     end
   end
 
   describe "POST finish_level" do
     it "returns http success" do
-      post :finish_level, user_id: user.id, level_number: levels.first.number
+      post :finish_level, user_id: @user.id, level_number: @levels.first.number
       expect(response).to have_http_status(:success)
       expect(JSON.parse response.body).to include({ 'status' => 'success' })
     end
 
     it "sets the high score" do
-      # Player completes a level with a score of 2000
-      post :finish_level, user_id: user.id, level_number: levels.first.number, score: 2000
+      post :finish_level, user_id: @user.id, level_number: @levels.first.number, score: 2000
+      # collect the previous high score.
+      older_score = @user.user_levels.find_by!(level: @levels.first).high_score
 
-      # Player completes the level again, this time with a score of 1000
-      post :finish_level, user_id: user.id, level_number: levels.first.number, score: 1000
+      post :finish_level, user_id: @user.id, level_number: @levels.first.number, score: 1000
 
-      # High score should be 2000
-      expect(user.user_levels.find_by!(level: levels.first).high_score).to eq(2000)
+      # test the high scores.
+      expect(@user.user_levels.find_by!(level: @levels.first).high_score).not_to eq(older_score)
+      expect(@user.user_levels.find_by!(level: @levels.first).high_score).to eq(1000)
     end
   end
 
